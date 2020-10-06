c86682d9d60cb09fea1979ffd1ba748528d69ee6
mobystudent <mobystudent@gmail.com>
2020-10-06 21:00:19 +0300

    Add favicon, footer block contacts, logic burger, logic submenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,29 @@
 $(window).on('load', () => {
-
+	showSubMenu();
+	showBurgerMenu();
 });
+
+function showSubMenu() {
+	$(".nav__link--drop").click(function(e) {
+		e.preventDefault();
+
+		$(".nav__submenu").slideUp();
+		$(this).next().css('display') == 'none' ? $(this).next().slideDown() :  $(this).next().slideUp();
+	});
+}
+
+function showBurgerMenu() {
+	$(".btn--burger").click(e => {
+		e.preventDefault();
+		
+		$(".burger").slideDown();
+		$(".wrapper").addClass("wrapper--hidden");
+	});
+
+	$(".btn--close").click(e => {
+		e.preventDefault();
+
+		$(".burger").slideUp();
+		$(".wrapper").removeClass("wrapper--hidden");
+	});
+}
