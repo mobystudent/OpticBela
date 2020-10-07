85935c2441777ff5ce98d7ad8d09bf942c69f650
mobystudent <mobystudent@gmail.com>
2020-10-07 14:28:41 +0300

    Add slider & logic show contacts in header

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
 $(window).on('load', () => {
 	showSubMenu();
 	showBurgerMenu();
+	slider();
+	showContactsHeader();
 });
 
 function showSubMenu() {
@@ -8,14 +13,14 @@ function showSubMenu() {
 		e.preventDefault();
 
 		$(".nav__submenu").slideUp();
-		$(this).next().css('display') == 'none' ? $(this).next().slideDown() :  $(this).next().slideUp();
+		$(this).next().css('display') == 'none' ? $(this).next().slideDown() : $(this).next().slideUp();
 	});
 }
 
 function showBurgerMenu() {
 	$(".btn--burger").click(e => {
 		e.preventDefault();
-		
+
 		$(".burger").slideDown();
 		$(".wrapper").addClass("wrapper--hidden");
 	});
@@ -25,5 +30,33 @@ function showBurgerMenu() {
 
 		$(".burger").slideUp();
 		$(".wrapper").removeClass("wrapper--hidden");
+	});
+}
+
+function slider() {
+	$("#mainSlider").slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		slidesToShow: 1,
+		easing: 'ease-in'
+	});
+}
+
+function showContactsHeader() {
+	$(".btn--contact").click(function(e) {
+		e.preventDefault();
+
+		const nameBlock = $(this).data("name");
+
+		$(".contacts__block--header").map((i, item) => {
+			if($(item).data('block') == nameBlock) {
+				if($(item).css('display') == 'none') {
+					$(".contacts__block--header").hide();
+					$(item).show();
+			 	} else {
+					$(item).hide();
+				}
+			}
+		});
 	});
 }
